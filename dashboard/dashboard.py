0b8e01a93cb8e9f27b6c067502c6bdc2028cdef5
andharsm <andhar.moenir@gmail.com>
2023-12-29 10:31:17 +0700

    add: dataset

---
+++ dashboard/dashboard.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import streamlit as st
+import urllib
+
+# Define your Streamlit app
+st.title("E-Commerce Public Data Analysis")
+
+# Daily Orders Purchase
+st.subheader("Daily Orders Purchase")
